0bc9bf8b775475dbe51ceac02d0df456e1458eb1
Jason <jasonmichaelroth@gmail.com>
2015-12-13 08:05:05 -0700

    more color profiles

--- profile/files/ssh.zsh
+++ profile/files/ssh.zsh
@@ -1,6 +1,29 @@
-ssh() {
-    local previous_profile=$ITERM_PROFILE
-    echo -e "\033]50;SetProfile=Danger\a"
-    /usr/bin/ssh "$@"
-    echo -e "\033]50;SetProfile=$previous_profile\a"
+function tabc() {
+  NAME=$1; if [ -z "$NAME" ]; then NAME="Default"; fi
+  # if you have trouble with this, change
+  # "Default" to the name of your default theme
+  echo -e "\033]50;SetProfile=$NAME\a"
 }
+
+function tab-reset() {
+    NAME="Default"
+    echo -e "\033]50;SetProfile=$NAME\a"
+    trap - INT EXIT
+}
+
+function colorssh() {
+    if [[ -n "$ITERM_SESSION_ID" ]]; then
+        trap "tab-reset" INT EXIT
+        if [[ "$*" =~ "pr.*" ]]; then
+            tabc Red
+        elif [[ "$*" =~ "stg.*" ]]; then
+            tabc Purple
+        else
+            tabc Blue
+        fi
+    fi
+    ssh $*
+}
+compdef _ssh tabc=ssh
+
+alias ssh="colorssh"
