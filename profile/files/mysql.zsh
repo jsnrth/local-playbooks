b5571f054a9c48da2a81f2927060b227cde91f16
Jason <jasonmichaelroth@gmail.com>
2015-12-13 08:11:13 -0700

    also change colors for cqlsh and mysql

---
+++ profile/files/mysql.zsh
@@ -0,0 +1,29 @@
+function tabc() {
+  NAME=$1; if [ -z "$NAME" ]; then NAME="Default"; fi
+  # if you have trouble with this, change
+  # "Default" to the name of your default theme
+  echo -e "\033]50;SetProfile=$NAME\a"
+}
+
+function tab-reset() {
+    NAME="Default"
+    echo -e "\033]50;SetProfile=$NAME\a"
+    trap - INT EXIT
+}
+
+function colormysql() {
+    if [[ -n "$ITERM_SESSION_ID" ]]; then
+        trap "tab-reset" INT EXIT
+        if [[ "$*" =~ "pr.*" ]]; then
+            tabc Red
+        elif [[ "$*" =~ "stg.*" ]]; then
+            tabc Purple
+        else
+            tabc Blue
+        fi
+    fi
+    mysql $*
+}
+compdef _mysql tabc=mysql
+
+alias mysql="colormysql"
